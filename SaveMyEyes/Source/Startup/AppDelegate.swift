36c4b68af6db48fa2f334e9342f9d297a96e294a
Maksym Omelchenko <masich.dev@gmail.com>
2020-04-14 10:05:33 +0300

    Fixed user notification sending when app is in foreground

--- SaveMyEyes/Source/Startup/AppDelegate.swift
+++ SaveMyEyes/Source/Startup/AppDelegate.swift
@@ -72,6 +72,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
         completionHandler()
     }
     
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
     private func setupNotifications() {
         AppNotificationManager.requestAuthorization()
         AppNotificationManager.removeAllNotifications()
